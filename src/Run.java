d856f7da238c08f784a225ad3a553deb638cfe79
TPriime <36935579+TPriime@users.noreply.github.com>
2018-10-27 16:12:41 +0100

    Update Run.java

--- src/Run.java
+++ src/Run.java
@@ -6,15 +6,6 @@ public class Run {
   	System.out.println("\t\t\t@Prime Soduku\t\t\t");
   	System.out.println();
 		PSoduku game = new PSoduku(3);
-		/*game.set(0, 0, 1, 0, 0, 8, 0, 7, 0, 0, 0);
-		game.set(0, 1, 7, 0, 0, 0, 0, 9, 0, 0, 0);
-		game.set(0, 2, 8, 9, 0, 3, 4, 0, 0, 0, 7);
-		game.set(1, 0, 0, 0, 0, 0, 7, 0, 6, 0, 4);
-		game.set(1, 1, 0, 4, 1, 0, 0, 0, 2, 9, 0);
-		game.set(1, 2, 2, 0, 6, 0, 3, 0, 0, 0, 0);
-		game.set(2, 0, 4, 0, 0, 0, 6, 9, 0, 1, 2);
-		game.set(2, 1, 0, 0, 0, 8, 0, 0, 0, 0, 5); 
-		game.set(2, 2, 0, 0, 0, 5, 0, 4, 0, 0, 3);*/
 		
 		game.set(0, 0, 5, 3, 0, 6, 0, 0, 0, 9, 8);
 		game.set(0, 1, 0, 7, 0, 1, 9, 5, 0, 0, 0);
@@ -25,7 +16,7 @@ public class Run {
 		game.set(2, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0);
 		game.set(2, 1, 0, 0, 0, 4, 1, 9, 0, 8, 0); 
 		game.set(2, 2, 2, 8, 0, 0, 0, 5, 0, 7, 9);
-		//game.displayValues2();
+	  
 		try{  game.fillUp();  }
 		catch(IndexOutOfBoundsException i) { 
 			System.out.println("\tCannot be solved!");
